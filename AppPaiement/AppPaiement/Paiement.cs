94117cbe6f0c0043d71b242ab793acd7a554e7df
fon <jolivet.fonkenfack@monboreal.ca>
2024-10-16 16:13:35 -0400

    ajout de modification

---
+++ AppPaiement/AppPaiement/Paiement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiement
+{
+    internal class Paiement
+    {
+        public double Montant { get; set; }
+        public string Description { get; set; }
+        public Paiement(double montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+        }
+
+        public void AfficherDetails()
+        {
+            Console.WriteLine($"Montant: {Montant}, Description: {Description}");
+        }
+
+    }
+}
