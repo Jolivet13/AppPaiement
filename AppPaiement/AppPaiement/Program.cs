2e81374212d00b0a798dec6a18ceb92c76de7137
fon <jolivet.fonkenfack@monboreal.ca>
2024-10-16 15:29:08 -0400

    ajout de modification

---
+++ AppPaiement/AppPaiement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AppPaiement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
