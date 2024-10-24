864c2d65b89ebce66989d9989bf97a9f8a67404e
fon <jolivet.fonkenfack@monboreal.ca>
2024-10-24 17:37:05 -0400

    ajout de la class fille paypal

---
+++ AppPaiement/AppPaiement/Paypal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiement
+{
+    internal class Paypal : Paiement
+    {
+        public string Courriel { get; set; }
+
+        public Paypal(decimal montant, string description, string email)
+            : base((double)montant, description)
+        {
+            Courriel = email;
+        }
+
+        public new virtual void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Email: {Courriel}");
+        }
+    }
+}
--- AppPaiement/AppPaiement/Program.cs
+++ AppPaiement/AppPaiement/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace AppPaiement
+﻿using System.Runtime.InteropServices.ObjectiveC;
+
+namespace AppPaiement
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var achat_jolivet = new CarteCredit(150.00m , " Achat de vêtements ",123456789);
+            var achat_jolivet = new CarteCredit(150.00m, " Achat de vêtements ", 123456789);
             var achat_steve = new CarteCredit(250.00m, " Achat de livres ", 234567890);
             var achat_fon = new CarteCredit(300.00m, " Achat de nourritures ", 345678901);
             List<CarteCredit> achats = new List<CarteCredit> { achat_jolivet, achat_steve, achat_fon };
@@ -14,7 +16,20 @@
                 achat.AfficherDetails();
                 Console.WriteLine();
 
+            }
+        
 
+
+
+            var paiement1 = new Paypal(750.00m, "Achat de logiciels", "kenfackjolivet@gmail.com");
+            var paiement2 = new Paypal(100.00m, "Paiement abonnement", "jolivet.fonkenfack@monboreal.ca");
+
+            List<Paypal> paiements = new List<Paypal> { paiement1, paiement2 };
+
+            foreach (var paiement in paiements)
+            {
+                paiement.AfficherDetails();
+                Console.WriteLine();
             }
         }
     }
