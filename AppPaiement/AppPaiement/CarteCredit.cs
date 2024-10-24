c7e51961baf4c9627ea4d0020e30b57790479461
fon <jolivet.fonkenfack@monboreal.ca>
2024-10-24 16:32:37 -0400

    ajout de la class CarteCredit

---
+++ AppPaiement/AppPaiement/CarteCredit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiement
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+        public CarteCredit(decimal montant, string description, int Carte)
+        : base((double)montant, description)
+        { NumeroCarte = Carte; }
+        public new virtual void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Numéro de Carte: {NumeroCarte}");
+
+        }
+    }
+}
--- AppPaiement/AppPaiement/Program.cs
+++ AppPaiement/AppPaiement/Program.cs
@@ -4,8 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            var achat_jolivet = new CarteCredit(150.00m , " Achat de vêtements ",123456789);
+            var achat_steve = new CarteCredit(250.00m, " Achat de livres ", 234567890);
+            var achat_fon = new CarteCredit(300.00m, " Achat de nourritures ", 345678901);
+            List<CarteCredit> achats = new List<CarteCredit> { achat_jolivet, achat_steve, achat_fon };
+
+            foreach (var achat in achats)
+            {
+                achat.AfficherDetails();
+                Console.WriteLine();
+
+
+            }
         }
     }
-    
 }
